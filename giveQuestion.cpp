2152181465920f36238bdee7dfeda1874a21b885
F74041056 <decade910831@gmail.com>
2016-04-03 15:30:38 +0800

    add giveQuestion.cpp to project1

---
+++ giveQuestion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstlib>
+#include <stime>
+#include "Sudoku.h"
+using namespace std;
+
+int main()
+{
+	Sudoku ss;
+	ss.giveQuestion();
+	return 0;
+}
